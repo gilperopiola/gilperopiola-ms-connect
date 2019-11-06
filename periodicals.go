e3d7df814ee9d0660f8578a74464c43aadea4d4b
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2019-11-06 14:37:48 -0300

    started periodicals

---
+++ periodicals.go
@@ -0,0 +1,16 @@
+package connect
+
+func GetPeriodicalsExpiringToday() (response string) {
+}
+
+func GetPeriodicalsToDoToday() (response string) {
+}
+
+func GetPeriodicalsToDoTomorrow() (response string) {
+}
+
+func GetAllPeriodicals() (response string) {
+}
+
+type Periodical struct {
+}
