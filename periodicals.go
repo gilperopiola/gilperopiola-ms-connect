73c2211c36e0f55713198497ab0f947db4482b8e
gilperopiola <ferra.main@gmail.com>
2019-11-09 12:50:23 -0300

    fix

--- periodicals.go
+++ periodicals.go
@@ -1,15 +1,19 @@
 package connect
 
 func GetPeriodicalsExpiringToday() (response string) {
+	return ""
 }
 
 func GetPeriodicalsToDoToday() (response string) {
+	return ""
 }
 
 func GetPeriodicalsToDoTomorrow() (response string) {
+	return ""
 }
 
 func GetAllPeriodicals() (response string) {
+	return ""
 }
 
 type Periodical struct {
