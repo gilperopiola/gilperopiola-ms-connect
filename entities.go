56eb53adb6b496afbf9c73e885a05d5ba29105b8
gilperopiola <ferra.main@gmail.com>
2020-02-04 23:21:29 -0300

    entities

---
+++ entities.go
@@ -0,0 +1,36 @@
+package connect
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/gilperopiola/frutils"
+)
+
+type Entity struct {
+	ID          int
+	Name        string
+	Description string
+	Kind        string
+	Importance  int
+	Status      int //1 = enabled, 2 = disabled
+	DateCreated time.Time
+}
+
+const msEntitiesURL = "http://localhost:9001"
+
+func GetEntitiesOfKind(kind string) ([]*Entity, error) {
+	endpointURL := msEntitiesURL + "/Entity?kind=" + kind
+
+	status, response := frutils.SendHTTPRequest("GET", endpointURL, "")
+
+	if status < 200 || status > 299 {
+		return []*Entity{}, errors.New(response)
+	}
+
+	entities := []*Entity{}
+	json.Unmarshal([]byte(response), entities)
+
+	return entities, nil
+}
