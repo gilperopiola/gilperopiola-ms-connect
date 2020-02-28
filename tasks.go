c765e851175fdc3f394dde16fdaa73b0fcc23d40
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2020-02-28 10:55:13 -0300

    Tasks & LC Login

---
+++ tasks.go
@@ -0,0 +1,75 @@
+package connect
+
+import (
+	"encoding/json"
+	"errors"
+	"time"
+
+	"github.com/gilperopiola/frutils"
+)
+
+type Task struct {
+	ID          int
+	Name        string
+	Description string
+	Importance  int
+	Duration    TaskDuration
+	DueDate     time.Time
+}
+
+type TaskDuration int
+
+const (
+	ExtraSmall TaskDuration = 1
+	Small      TaskDuration = 2
+	Medium     TaskDuration = 3
+	Large      TaskDuration = 4
+	ExtraLarge TaskDuration = 5
+)
+
+type User struct {
+	Token string
+}
+
+const lyfeCompanyonURL = "http://localhost:9001"
+
+func GetLyfeCompanyonToken(username string, password string) string {
+	endpointURL := lyfeCompanyonURL + "/Login"
+
+	httpRequestBody := `{
+		"username": "` + username + `",
+		"password": "` + password + `"
+	}`
+
+	status, response := frutils.SendHTTPRequest("POST", endpointURL, httpRequestBody)
+
+	if status < 200 || status > 299 {
+		return ""
+	}
+
+	user := &User{}
+	json.Unmarshal([]byte(response), user)
+
+	return user.Token
+}
+
+func CreateTask(name string, importance int, duration int, token string) (*Task, error) {
+	endpointURL := lyfeCompanyonURL + "/Tasks"
+
+	httpRequestBody := `{
+		"name": "` + name + `",
+		"duration": ` + frutils.ToString(duration) + `,
+		"importance": ` + frutils.ToString(importance) + `
+	}`
+
+	status, response := frutils.SendHTTPRequestWithToken("POST", endpointURL, httpRequestBody, token)
+
+	if status < 200 || status > 299 {
+		return &Task{}, errors.New(response)
+	}
+
+	task := &Task{}
+	json.Unmarshal([]byte(response), task)
+
+	return task, nil
+}
